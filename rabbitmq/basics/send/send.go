3c940a93a2259c10ccab9ba3628ebb80fbf09c49
RITANKAR SAHA <ritankar.saha786@gmail.com>
2025-01-07 22:48:53 +0530

    RabbitMQ final commits working

--- rabbitmq/basics/send/send.go
+++ rabbitmq/basics/send/send.go
@@ -1,15 +1,17 @@
 package main
 
 import (
+	"bufio"
 	"context"
+	"fmt"
 	"log"
+	"os"
 	"time"
 	amqp "github.com/rabbitmq/amqp091-go"
 )
 
 func main() {
-
-	//connecting to the rabbitmq server
+	// Connect to the RabbitMQ server
 	conn, err := amqp.Dial("amqp://guest:guest@localhost:5672/")
 	failOnError(err, "Failed to connect to RabbitMQ")
 	defer conn.Close()
@@ -18,37 +20,46 @@ func main() {
 	failOnError(err, "Failed to open a channel")
 	defer ch.Close()
 
-	//declaring a queue to send the message to the queue
+	// Declare a queue to send the message to the queue
 	q, err := ch.QueueDeclare(
-		"hello", //name
-		false,   //durable
-		false,   //delete when unused
-		false,   //excluive
-		false,   //no-wait
-		nil,     //arguments
+		"hello", // name
+		false,   // durable
+		false,   // delete when unused
+		false,   // exclusive
+		false,   // no-wait
+		nil,     // arguments
 	)
-
 	failOnError(err, "Failed to declare a queue")
 
-	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
-	defer cancel()
+	reader := bufio.NewReader(os.Stdin)
+	for {
+		fmt.Print("Enter your message (type 'exit' to quit): ")
+		body, _ := reader.ReadString('\n')
+		body = body[:len(body)-1] 
 
-	body := "Hello my name is Ritankar"
-	err = ch.PublishWithContext(
-		ctx,
-		"",     //exchange
-		q.Name, //routing key
-		false,  //mandatory
-		false,  //immediate
-		amqp.Publishing{
-			ContentType: "text/plain",
-			Body:        []byte(body),
-		},
-	)
+		if body == "exit" {
+			fmt.Println("Exiting...")
+			break
+		}
 
-	failOnError(err, "Failed to publish a message")
-	log.Printf(" [x] Sent %s\n", body)
+		ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+		defer cancel()
 
+		err = ch.PublishWithContext(
+			ctx,
+			"",     // exchange
+			q.Name, // routing key
+			false,  // mandatory
+			false,  // immediate
+			amqp.Publishing{
+				ContentType: "text/plain",
+				Body:        []byte(body),
+			},
+		)
+		failOnError(err, "Failed to publish a message")
+
+		log.Printf(" [x] Sent: %s\n", body)
+	}
 }
 
 func failOnError(err error, msg string) {
